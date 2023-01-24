8cacb438679fc6f4084c76521789d565b9303e96
Wojciech <szmelczerczykwojciech@gmail.com>
2023-01-24 16:51:22 +0100

    api communication with server side of application work

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableHighlight,
+} from "react-native";
+import axios from "axios";
 
 export default function App() {
+  const [data, setData] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+
+  const getPredictions = async () => {
+    const response = await axios.post("http://localhost:3000/", { imageUrl });
+
+    console.log(response);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
       <StatusBar style='auto' />
+      <TextInput
+        style={{
+          height: 40,
+          width: 300,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        onChange={(val) => setImageUrl(val.nativeEvent.text)}
+      />
+      <TouchableHighlight onPress={getPredictions}>
+        <Text>Press this button to submit editing</Text>
+      </TouchableHighlight>
     </View>
   );
 }
