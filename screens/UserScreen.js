f6d1fc496dcada60d27f723339cd576bafe04ec0
Wojciech <szmelczerczykwojciech@gmail.com>
2023-01-26 21:59:48 +0100

    user logout succesfully implemented

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
 
-const UserScreen = () => {
+const UserScreen = ({navigation}) => {
+  const signOutUser = async () => {
+    try {
+      await signOut(auth);
+      navigation.navigate("Login");
+    } catch (err) {}
+  };
+
   return (
-    <View>
-      <Text>UserScreen</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={signOutUser}>
+        <Text>Sign Out</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default UserScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+  },
+});
