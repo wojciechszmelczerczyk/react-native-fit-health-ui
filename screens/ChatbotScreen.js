ada478ba2c36c86fa89263a7d68778dd63b8f35b
Wojciech <szmelczerczykwojciech@gmail.com>
2023-02-15 14:15:28 +0100

    chatbot library used to create chat ui, working on integration with openai text completion

--- screens/ChatbotScreen.js
+++ screens/ChatbotScreen.js
@@ -1,25 +1,59 @@
 import React from "react";
-import { useRef } from "react";
+import { useContext, useState, useCallback, useEffect, useRef } from "react";
 import { StyleSheet, View } from "react-native";
 import { getAnswer } from "../services/ChatbotService";
-import LoginScreen from "react-native-login-screen";
-
+import { GiftedChat } from "react-native-gifted-chat";
+import { UserContext } from "../context/UserContext";
 const ChatbotScreen = () => {
-  const handleInput = async () => {
-    const res = await getAnswer(prompt.current);
+  const [user] = useContext(UserContext);
+  const [messages, setMessages] = useState([]);
+
+  const handleInput = async (text) => {
+    const chatBotRes = await getAnswer(text);
+
+    // TODO: make chatbot response appear in messages
   };
 
-  const prompt = useRef(null);
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: `Hello ${user.name || user.email}. How can I help you?`,
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: "Fitness ChatBot",
+          avatar: "",
+        },
+      },
+    ]);
+  }, []);
+
+  // const prompt = useRef(null);
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
 
   return (
-    <View>
-      <LoginScreen
-        loginButtonStyle={{ backgroundColor: "#6432ff" }}
-        onLoginPress={handleInput}
-        disableDivider
-        disableSocialButtons
-        onPasswordChange={(value) => (prompt.current = value)}
-        style={{ backgroundColor: "#fff" }}
+    <View style={{ flex: 1, marginBottom: 55 }}>
+      <GiftedChat
+        messages={messages}
+        onSend={async (messages) => {
+          // call function
+          onSend(messages);
+
+          // intercept input text
+          const [textMessage] = messages;
+
+          const { text } = textMessage;
+
+          await handleInput(text);
+        }}
+        user={{
+          _id: 2,
+        }}
       />
     </View>
   );
